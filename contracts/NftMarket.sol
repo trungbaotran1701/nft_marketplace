b14d7bc78c090b4931e349409852648e1572dc5e
CakChyn <trungbaotran1701@gmail.com>
2022-12-04 00:08:48 +0700

    done mint token

--- contracts/NftMarket.sol
+++ contracts/NftMarket.sol
@@ -1,10 +1,35 @@
-// contracts/MyNFT.sol
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract NftMarket is ERC721 {
-    constructor() ERC721("TrungChanNFT", "TCNFT") {
+contract NftMarket is ERC721URIStorage {
+    using Counters for Counters.Counter;
+
+    Counters.Counter private _listedItems;
+    Counters.Counter private _tokenIds;
+
+    mapping(string => bool) private _existsTokenURI;
+
+    constructor() ERC721("TrungChanNFT", "TCNFT") {}
+
+    function mintToken(string memory tokenURI) public payable returns (uint256) {
+        require(!checkTokenURIs(tokenURI), 'TokenURI has been existed');
+        _listedItems.increment();
+        _tokenIds.increment();
+
+        uint newTokenId = _tokenIds.current();
+
+        _safeMint(msg.sender, newTokenId);
+        _setTokenURI(newTokenId, tokenURI);
+        _existsTokenURI[tokenURI] = true;
+
+        return newTokenId;
     }
+
+    function checkTokenURIs(string memory tokenURI) public view returns (bool) {
+        return _existsTokenURI[tokenURI] == true;
+    }
+
 }
--- truffle-config.js
+++ truffle-config.js
@@ -10,7 +10,7 @@ module.exports = {
 
   compilers: {
     solc: {
-      version: "0.8.0", // Fetch exact version from solc-bin (default: truffle's version)
+      version: "0.8.7", // Fetch exact version from solc-bin (default: truffle's version)
     },
   },
 };
